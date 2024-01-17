958239332b10b990e2cbab9119693602e2a5e63b
Bryan Gruneberg <hello@bryangruneberg.com>
2024-01-17 09:05:00 -0600

    Environment

--- lagoon/scripts/next.sh
+++ lagoon/scripts/next.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Loading environment variables from .env and friends
+source /lagoon/entrypoints/50-dotenv.sh
+
+# Generate some additional enviornment variables
+source /lagoon/entrypoints/55-generate-env.sh
+
+export
+
 echo "$LAGOON_ENVIRONMENT_TYPE: NextJS is starting"
 
 if [ "$LAGOON_ENVIRONMENT_TYPE" == "production" ]; then
